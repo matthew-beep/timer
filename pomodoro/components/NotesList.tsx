64b1b46b6249f5e0ae9f67ad196737f27e54befa
Matthew <matthew.herradura@gmail.com>
2026-01-09 19:02:52 -0800

    working on noteslist

--- pomodoro/components/NotesList.tsx
+++ pomodoro/components/NotesList.tsx
@@ -2,13 +2,17 @@
 import { motion } from 'framer-motion'
 import { useNotesStore } from '@/store/useNotes';
 // TODO: Refactor to use react-rnd instead of draggable
+
+import { useMemo } from 'react'
+import { generateHTML } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
 export default function NotesList() {
   const notes = useNotesStore((s) => s.notes);
 
   return (
     // Pass nodeRef to Draggable and attach the ref to the actual DOM node child
     <motion.div 
-      className="top-0 right-0 absolute inset-0 z-10 w-24 h-full pointer-events-none bg-[#0a1929]/50 backdrop-blur-2xl border-l border-white/10"
+      className="top-0 right-0 absolute inset-0 z-10 w-1/2 flex flex-col h-full pointer-events-none bg-[#0a1929]/50 backdrop-blur-2xl border-l border-white/10"
       initial={{
         opacity: 0,
         x: -300,
@@ -26,9 +30,14 @@ export default function NotesList() {
         {notes.map((note) => (
           <div 
             key={note.id}
-            className="p-2 border-b border-white/10"
+            className="p-2 border-2 w-auto flex border-white/10 flex-col"
           >
-            <p className="text-sm">{note.id}</p>
+            <p className="text-sm border-2">{note.id}</p>
+            <div 
+              className="ml-2 text-sm border-2 overflow-auto max-h-32 prose prose-sm prose-invert tiptap"
+              dangerouslySetInnerHTML={{ __html: generateHTML(note.text, [StarterKit]) }}
+            >
+            </div>
           </div>
         ))}
 
